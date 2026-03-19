1620eee4b648926c448d49d0b0b2e8d9f37858b1
Poovitha18 <97659582+Poovitha18@users.noreply.github.com>
2026-03-19 17:05:56 +0530

    Fix formatting and update ingredients handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,20 +33,19 @@ ingredients_list = st.multiselect(
     my_dataframe , 
     max_selections=5
 )
-
 if ingredients_list:
   #st.write(ingredients_list)
   #st.text(ingredients_list)
 
-  ingredients_string = ''
-  for fruit_chosen in ingredients_list:
+   ingredients_string = ''
+   for fruit_chosen in ingredients_list:
       ingredients_string+=fruit_chosen + ' '
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
       sf_df = st.dataframe(data = smoothiefroot.response.json(),use_container_width=True)
 
   #st.write(ingredients_string)    
 
-  my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+   my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string +"""','"""+name_on_order+ """')"""
     
 
